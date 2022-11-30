0da4c2224fabe085c1bc02f64543e1c512588cb7
Miguel Lopez <ing.miguellv@gmail.com>
2022-11-30 10:11:51 -0600

    agrego select para criptomonedas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import styled from "@emotion/styled"
 import useSelectMonedas from "../hooks/useSelectMonedas"
 import {monedas} from '../data/monedas'
@@ -21,11 +22,37 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+  const [criptos, setCriptos] = useState([])
+
   const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas)
+  const [criptomoneda, SelectCriptomoneda] = useSelectMonedas('Elige tu criptomoneda', criptos)
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+  
+      const response = await fetch(url)
+      const resultado = await response.json()
+
+      const arrayCriptos = resultado.Data.map(cripto => {
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName
+        }
+        return objeto
+      })
+
+      setCriptos(arrayCriptos)
+    }
+
+    consultarAPI()
+  },[])
 
   return (
     <form>
       <SelectMonedas/>
+
+      <SelectCriptomoneda/>
 
       <InputSubmit
         type="submit"
